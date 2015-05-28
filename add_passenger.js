2c486c25f7572f493fb856fd0c32bcb39b0a0160
Sana Javed <sanajaved7@gmail.com>
2015-05-27 22:44:26 -0400

    Added train passenger program

---
+++ add_passenger.js
@@ -0,0 +1,19 @@
+// array and function to keep track of train passenger list
+
+function addPassenger (name, list) {
+    if (list.length == 0) {
+        list.push(name);
+    }
+
+    else {
+        for (var i = 0; i < list.length; i++) {
+            if(list[i] == undefined) {
+                list[i] = name;
+                return list;
+            }
+            else if (i == list.length -1) {
+                list.push(name);
+            }
+        }
+    }
+}
